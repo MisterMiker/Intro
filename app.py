2a2768b8569c5776e9e3380aa85cdbba28b9a8ac
MisterMiker <miguelrlopez2005@gmail.com>
2025-08-28 12:54:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ from PIL import Image
 st.title("Mi primera App!!")
 
 image = Image.open('Lol.jpg')
-
 st.image(image, caption='Papu')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
